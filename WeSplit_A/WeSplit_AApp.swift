9596f75d3835a40211bf8fa7f7b5375d94d4cce6
RegulusSpica1 <51476865+aayanshsingh@users.noreply.github.com>
2022-12-17 16:04:15 +0530

    Initial Commit

---
+++ WeSplit_A/WeSplit_AApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit_AApp.swift
+//  WeSplit_A
+//
+//  Created by RegulusSpica1 on 17/12/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit_AApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
